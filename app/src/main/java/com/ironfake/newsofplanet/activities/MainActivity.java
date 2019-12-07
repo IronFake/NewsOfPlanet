70a456b6952b305adba5dcdb10474eb6c140102b
IronFake <54504064+IronFake@users.noreply.github.com>
2019-12-07 20:34:57 +0500

    created search, add recyclerView with basic categories

--- app/src/main/java/com/ironfake/newsofplanet/activities/MainActivity.java
+++ app/src/main/java/com/ironfake/newsofplanet/activities/MainActivity.java
@@ -8,6 +8,10 @@ import android.app.DownloadManager;
 import android.app.VoiceInteractor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,6 +20,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.ironfake.newsofplanet.R;
+import com.ironfake.newsofplanet.data.CategoryNewsAdapter;
 import com.ironfake.newsofplanet.data.NewsAdapter;
 import com.ironfake.newsofplanet.model.News;
 
@@ -33,7 +38,14 @@ public class MainActivity extends AppCompatActivity {
     private NewsAdapter newsAdapter;
     private ArrayList<News> newsArrayList;
 
+    private RecyclerView categoryNewsRecyclerView;
+    private CategoryNewsAdapter categoryNewsAdapter;
+    private ArrayList<String> categoriesArrayList;
+    private final String[] categories = {"Business", "Entertainment", "General", "Health", "Sports", "Technology"};
+
     private RequestQueue requestQueue;
+
+    private SearchView searchView;
 
 
     @Override
@@ -41,18 +53,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                getNews("q=" + query.trim());
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        categoriesArrayList = new ArrayList<>();
+        for (int i = 0; i < categories.length; i++) {
+            categoriesArrayList.add(categories[i]);
+        }
+        categoryNewsRecyclerView = findViewById(R.id.categoryNewsRecyclerView);
+        categoryNewsRecyclerView.hasFixedSize();
+        categoryNewsRecyclerView.setLayoutManager(new LinearLayoutManager(this,
+                LinearLayoutManager.HORIZONTAL, false));
+        categoryNewsAdapter = new CategoryNewsAdapter(MainActivity.this, categoriesArrayList);
+        categoryNewsAdapter.setOnUserClickListener(new CategoryNewsAdapter.OnUserClickListener() {
+            @Override
+            public void onUserClick(int position) {
+                getNews("category=" +categories[position]);
+            }
+        });
+        categoryNewsRecyclerView.setAdapter(categoryNewsAdapter);
+
         newsRecyclerView = findViewById(R.id.newsRecyclerView);
         newsRecyclerView.hasFixedSize();
         newsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         newsArrayList = new ArrayList<>();
         requestQueue = Volley.newRequestQueue(this);
-        gerNews();
+        getNews("");
 
     }
 
-    private void gerNews() {
-        Log.d("News", "start");
-        String url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + API_KEY;
+    private void getNews(String query) {
+        newsArrayList.clear();
+        String url;
+        if (query.contains("q=")){
+            url = "https://newsapi.org/v2/everything?q=" +query + "&apiKey=" + API_KEY;
+        } else if (query.contains("category=")){
+            url = "https://newsapi.org/v2/top-headlines?" +query + "&apiKey=" + API_KEY;
+        } else {
+            url = "https://newsapi.org/v2/top-headlines?category=general&apiKey=" + API_KEY;
+        }
 
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
@@ -60,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         try {
                             JSONArray jsonArray = response.getJSONArray("articles");
-                            for (int i = 0; i < 10; i++) {
+                            for (int i = 0; i < jsonArray.length(); i++) {
                                 JSONObject jsonObject = jsonArray.getJSONObject(i);
 
                                 String title = jsonObject.getString("title");
---
+++ app/src/main/java/com/ironfake/newsofplanet/data/CategoryNewsAdapter.java
@@ -0,0 +1,77 @@
+package com.ironfake.newsofplanet.data;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ironfake.newsofplanet.R;
+
+import java.util.ArrayList;
+
+public class CategoryNewsAdapter extends RecyclerView.Adapter<CategoryNewsAdapter.CategoryNewsViewHolder> {
+
+    private Context context;
+    private ArrayList<String> categories;
+    private OnUserClickListener listener;
+
+    public interface OnUserClickListener{
+        void onUserClick(int position);
+    }
+    public void setOnUserClickListener(OnUserClickListener listener){
+        this.listener = listener;
+    }
+
+    public CategoryNewsAdapter(Context context, ArrayList<String> categories) {
+        this.context = context;
+        this.categories = categories;
+    }
+
+    @NonNull
+    @Override
+    public CategoryNewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.category_item_recycler_view,
+                parent, false);
+
+        return new CategoryNewsViewHolder(view, listener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryNewsViewHolder holder, int position) {
+        String category = categories.get(position);
+
+        holder.categoryTextView.setText(category);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+
+    public class CategoryNewsViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView categoryTextView;
+
+        public CategoryNewsViewHolder(@NonNull View itemView, final OnUserClickListener listener) {
+            super(itemView);
+
+            categoryTextView = itemView.findViewById(R.id.categoryTextView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onUserClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
